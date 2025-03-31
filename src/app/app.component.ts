ef84f33b166481f670139f6cc3bab76b7225ff96
Brad Moffat <brad@moffat.me.uk>
2025-03-31 09:03:45 +0100

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { FormsModule } from '@angular/forms';
+import { MatGridList, MatGridTile } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, MatButtonToggleModule, MatGridList, MatGridTile, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-material-theme';
+  theme = 'light';
+
+
 }
